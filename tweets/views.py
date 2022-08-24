5e5bbca782e3923afcac7c760263dfdf58d99e5e
007seann <007seann@gmail.com>
2022-08-24 14:58:02 +0100

    Handling Dynamic Routing

--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,18 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-  print(args, kwargs)
   return HttpResponse("<h1>HELLO World!</h1>")
 
 def room1_view(request, *args, **kwargs):
   return HttpResponse("<h1>This is sean's private room!</h1>")
 
 def tweet_datail_view(request, tweet_id, *args, **kwargs):
-  return HttpResponse(f"<h1>HELLO {tweet_id}!</h1>")
+  try:
+    obj = Tweet.objects.get(id=tweet_id)
+  except:
+    raise Http404
+  return HttpResponse(f"<h1>HELLO {tweet_id} --- {obj.content}!</h1>")
