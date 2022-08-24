ecc2b926d720f3aff92202088583bd6a3984da40
007seann <007seann@gmail.com>
2022-08-24 14:46:34 +0100

    Intro to URL Routing and Dynamic Routing

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+  print(args, kwargs)
+  return HttpResponse("<h1>HELLO World!</h1>")
+
+def room1_view(request, *args, **kwargs):
+  return HttpResponse("<h1>This is sean's private room!</h1>")
+
+def tweet_datail_view(request, tweet_id, *args, **kwargs):
+  return HttpResponse(f"<h1>HELLO {tweet_id}!</h1>")
