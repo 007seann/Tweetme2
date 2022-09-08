c51b42acde935e077193dc5403162844a52febd7
007seann <007seann@gmail.com>
2022-09-08 18:24:33 +0100

    Safe URL Redirect

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-opd*9@v4j8$4mxb2(sdyd#&7sp8)m(8sqf3hsy%rzh_n+5l4&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
