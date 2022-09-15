a1baec93976e355f8eafed81e3585920b0d52b63
007seann <007seann@gmail.com>
2022-09-15 15:25:51 +0100

    41. Delete Tweet API View

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path # url()
 
-from tweets.views import home_view, room1_view, tweet_datail_view, tweet_list_view, tweet_create_view
+from tweets.views import (home_view,
+                          room1_view,
+                          tweet_datail_view,
+                          tweet_list_view,
+                          tweet_create_view,
+                          tweet_delete_view)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
     path('create-tweet', tweet_create_view),
     path('tweets', tweet_list_view),
     path('tweets/<int:tweet_id>/', tweet_datail_view),
+    path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
 ]
+ 
